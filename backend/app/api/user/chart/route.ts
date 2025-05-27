0b5cde80eded73569b64bf07e17a29198673a7b7
heruarbianto <heru_arbianto@teknokrat.ac.id>
2025-05-27 12:01:06 +0700

    master: Membuat POst API menambahkan item ke keranjang

--- backend/app/api/user/chart/route.ts
+++ backend/app/api/user/chart/route.ts
@@ -99,3 +99,115 @@ export const GET = async (request: NextRequest) => {
     );
   }
 };
+
+
+// Post API Tambahkan Menu Ke Keranjang
+export const POST = async (request: NextRequest) => {
+   // Verifikasi token
+   const decoded: any = await verifyJWT(request);
+
+   // Jika gagal, decoded akan jadi Response (dari middleware)
+   if (decoded instanceof Response) {
+     return decoded;
+   }
+  try {
+    const idUser = Number(decoded.id);
+    const body = await request.json();
+    const {idMenu, quantity } = body;
+
+    if (!idMenu || !quantity) {
+      return NextResponse.json(
+        {
+          metadata: {
+            error: 1,
+            message: "Semua field (idMenu, quantity) wajib diisi.",
+          },
+        },
+        { status: 400 }
+      );
+    }
+
+    const menu = await prisma.tb_menu.findUnique({
+      where: { id: idMenu },
+    });
+
+    if (!menu) {
+      return NextResponse.json(
+        { error: "Menu dengan ID tersebut tidak ditemukan." },
+        { status: 404 }
+      );
+    }
+
+    const existingCartItems = await prisma.tb_keranjang.findMany({
+      include: {
+        tb_user: true,
+        tb_menu: true,
+      },
+    });
+
+    const cartItemFound = existingCartItems.find(
+      (cart: any) =>
+        cart.idUser === idUser &&
+        cart.idMenu === idMenu &&
+        cart.status === "FALSE"
+    );
+
+    if (cartItemFound) {
+      const oldQuantity = cartItemFound.quantity;
+      const menuPrice = cartItemFound.tb_menu.harga;
+      const newQuantity = quantity + oldQuantity;
+
+      await prisma.tb_keranjang.updateMany({
+        where: {
+          idUser,
+          idMenu,
+          status: "FALSE",
+        },
+        data: {
+          quantity: newQuantity,
+          total: newQuantity * menuPrice,
+        },
+      });
+
+      return NextResponse.json(
+        {
+          metadata: {
+            error: 0,
+            message: "Item keranjang berhasil diperbarui.",
+          },
+        },
+        { status: 200 }
+      );
+    } else {
+      await prisma.tb_keranjang.create({
+        data: {
+          idUser,
+          idMenu,
+          quantity,
+          total: quantity * menu.harga,
+        },
+      });
+
+      return NextResponse.json(
+        {
+          metadata: {
+            error: 0,
+            message: "Item berhasil ditambahkan ke keranjang.",
+          },
+        },
+        { status: 201 }
+      );
+    }
+  } catch (e: any) {
+    console.error("Error:", e);
+    return NextResponse.json(
+      {
+        metadata: {
+          error: 1,
+          message: "Terjadi kesalahan pada server.",
+        },
+      },
+      { status: 500 }
+    );
+  }
+};
