3150fb7a8294d955e5fa308b85e3d6e4e2e3da07
heruarbianto <heru_arbianto@teknokrat.ac.id>
2025-06-09 11:53:12 +0700

    Master : Membuat GET API Menampilkan Daftar Pesanan User berdasarkan id User Yang Login

--- backend/app/api/pemesanan/route.ts
+++ backend/app/api/pemesanan/route.ts
@@ -1,7 +1,7 @@
 // /api/pemesanan/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from "../general";
-import { verifyJWT } from "@/utils/verifyJWT";
+import { verifyJWT, verifyAdminJWT} from "@/utils/verifyJWT";
 
 export async function POST(req: NextRequest) {
   // Verifikasi token
@@ -92,4 +92,90 @@ export async function POST(req: NextRequest) {
       { status: 500 }
     );
   }
+}
+
+
+
+export async function GET(req: NextRequest) {
+    // Verifikasi token
+    const decoded: any = await verifyJWT(req);
+
+    if (decoded instanceof Response) {
+        return decoded;
+    }
+
+    try {
+        const idUser = decoded.id;
+
+        // Ambil semua pemesanan untuk user ini
+        const pemesanan = await prisma.tb_pemesanan.findMany({
+            orderBy:{
+                createdAt: 'desc'
+            },
+            where: {
+                idUser
+            },
+            include: {
+                user: {
+                    select: {
+                        namaLengkap: true,
+                        username: true,
+                        email: true,
+                        noHp: true,
+                        alamat: true
+                    }
+                },
+                detail_pemesanan: {
+                    include: {
+                        tb_keranjang: {
+                            include: {
+                                tb_menu: true
+                            }
+                        }
+                    }
+                },
+                tb_meja: true,
+            },
+        });
+
+        if (pemesanan.length < 1) {
+             return NextResponse.json(
+        {
+          metadata: {
+            error: 0,
+            message:
+              "Pesanan Masih Kosong, Silahkan Lakukan Pemesanan!!!",
+          },
+          dataChart: [],
+        },
+        {
+          status: 200,
+        }
+      );
+        }
+
+        return NextResponse.json(
+            {
+                metadata: {
+                    error: 0,
+                    message: "Pesanan Berhasil Ditampilkan!"
+                },
+                dataPemesanan: pemesanan
+            },
+            {
+                status: 200
+            }
+        );
+    } catch (error) {
+        console.error("Server error saat mengambil data pemesanan:", error);
+        return NextResponse.json(
+            {
+                metadata: {
+                    error: 1,
+                    message: "Terjadi kesalahan server!"
+                }
+            },
+            { status: 500 }
+        );
+    }
 }
